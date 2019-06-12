be1f293df9a79f0a7a7bb4b5143fa47facb2ef6b
Lake609 <lee@smail.hunnu.edu.cn>
2019-06-12 20:39:27 +0800

    translation from English to Chinese

--- research/im2txt/run_inference.py
+++ research/im2txt/run_inference.py
@@ -21,6 +21,11 @@ from __future__ import print_function
 import math
 import os
 
+import http.client
+import hashlib
+import json
+import urllib
+import random
 
 import tensorflow as tf
 
@@ -42,6 +47,37 @@ tf.flags.DEFINE_string("input_files", r"../../pic/cat.jpg",
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+
+def translate(content):
+    appid = '20190607000305603'
+    secretKey = '_Gt2mMy7IGvlerFUJMTZ'
+    httpClient = None
+    myurl = '/api/trans/vip/translate'
+    q = content
+    fromLang = 'en'  # 源语言
+    toLang = 'zh'  # 翻译后的语言
+    salt = random.randint(32768, 65536)
+    sign = appid + q + str(salt) + secretKey
+    sign = hashlib.md5(sign.encode()).hexdigest()
+    myurl = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(
+        q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(
+        salt) + '&sign=' + sign
+
+    try:
+        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+        httpClient.request('GET', myurl)
+        # response是HTTPResponse对象
+        response = httpClient.getresponse()
+        jsonResponse = response.read().decode("utf-8")  # 获得返回的结果，结果为json格式
+        js = json.loads(jsonResponse)  # 将json格式的结果转换字典结构
+        dst = str(js["trans_result"][0]["dst"])  # 取得翻译后的文本结果
+        return dst
+        #print(dst)  # 打印结果
+    except Exception as e:
+        print(e)
+    finally:
+        if httpClient:
+            httpClient.close()
 
 def main(_):
   # Build the inference graph.
@@ -79,7 +115,10 @@ def main(_):
         # Ignore begin and end words.
         sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
         sentence = " ".join(sentence)
+        content = translate(sentence)
+        print("  %d) %s (p=%f)" % (i, content, math.exp(caption.logprob)))
         print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
+        print('________________________________________________________')
   #os.system("pause")
 
 if __name__ == '__main__':
